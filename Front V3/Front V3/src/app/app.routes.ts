053d4255688e130b7e42e876a79a37ea3079152d
ivan1808 <a19201066@alumnos.uady.mx>
2026-05-13 18:29:17 -0600

    routes

--- Front V3/Front V3/src/app/app.routes.ts
+++ Front V3/Front V3/src/app/app.routes.ts
@@ -8,56 +8,8 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./features/login/pages/login').then(c => c.LoginComponent)
+    loadChildren: () => import('./features/login/login-routes').then(m => m.LOGIN_ROUTES)
   },
-   {
-        path: 'admin', component: AdminLayout,
-        data: {role: 'admin'},
-        children: [
-            {
-                path: '',
-                redirectTo: 'dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'dashboard',
-                loadChildren: () =>
-                    import('./features/dashboard/dashboard-routes')
-                        .then(m => m.DASHBOARD_ROUTES)
-            },
-            {
-                path: 'transport',
-                loadChildren: () =>
-                    import('./features/transport/transport-routes')
-                        .then(m => m.TRANSPORT_ROUTES)
-            },
-            {
-                path: 'employee',
-                loadChildren: () =>
-                    import('./features/employes/employee-routes')
-                        .then(m => m.EMPLOYEE_ROUTES)
-            },
-            {
-                path: 'trips',
-                loadChildren: () =>
-                    import('./features/trips/trips-routes')
-                        .then(m => m.TRIPS_ROUTES)
-            },
-            {
-                path: 'logistics',
-                loadChildren: () =>
-                    import('./features/logistics/logistic-routes')
-                        .then(m => m.LOGISTICS_ROUTES)
-            }
-        ]
-    },
-  // Ruta por defecto: raíz a login
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  // EL COMODÍN SIEMPRE DEBE IR AL FINAL
   {
     path: '**',
     redirectTo: 'login'
