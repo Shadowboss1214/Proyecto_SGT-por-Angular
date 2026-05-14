006f355669631995812cb90fd38884b58b81be06
ivan1808 <a19201066@alumnos.uady.mx>
2026-05-13 18:22:48 -0600

    arreglo

--- Front V3/Front V3/src/app/app.routes.ts
+++ Front V3/Front V3/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/pages/login';
-import { TransportListComponent } from './features/transport';
-import { TripsListComponent } from './features/trips/pages/trips-list/trips-list';
-import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard';
-import { AdminLayout } from './layouts/admin-layout/admin-layout';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
   {
     path: 'login',
     loadChildren: () => import('./features/login/login-routes').then(m => m.LOGIN_ROUTES)
