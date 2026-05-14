556db4404cc0ce6e028614bd732c1cbacf248b57
Gael Salazar <gaelsalazar2511@gmail.com>
2026-05-13 23:10:44 -0600

    Agregando a usuarios permisos

--- Front V3/Front V3/src/app/app.routes.ts
+++ Front V3/Front V3/src/app/app.routes.ts
@@ -1,16 +1,88 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/pages/login';
+import { TransportListComponent } from './features/transport';
+import { TripsListComponent } from './features/trips/pages/trips-list/trips-list';
+import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
+import { DriverLayout } from './layouts/driver-layout/driver-layout';
 
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./features/login/login-routes').then(c => c.LOGIN_ROUTES)
+    loadComponent: () => import('./features/login/pages/login').then(c => c.LoginComponent)
   },
+   {
+        path: 'admin', component: AdminLayout,
+        data: {role: 'admin'},
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
+                loadChildren: () =>
+                    import('./features/dashboard/dashboard-routes')
+                        .then(m => m.DASHBOARD_ROUTES)
+            },
+            {
+                path: 'transport',
+                loadChildren: () =>
+                    import('./features/transport/transport-routes')
+                        .then(m => m.TRANSPORT_ROUTES)
+            },
+            {
+                path: 'employee',
+                loadChildren: () =>
+                    import('./features/employes/employee-routes')
+                        .then(m => m.EMPLOYEE_ROUTES)
+            },
+            {
+                path: 'trips',
+                loadChildren: () =>
+                    import('./features/trips/trips-routes')
+                        .then(m => m.TRIPS_ROUTES)
+            },
+            {
+                path: 'logistics',
+                loadChildren: () =>
+                    import('./features/logistics/logistic-routes')
+                        .then(m => m.LOGISTICS_ROUTES)
+            }
+        ]
+    },
+    {
+            path: 'driver', component: DriverLayout,
+            data: {role: 'driver'},
+            children: [
+                {
+                    path: '',
+                    redirectTo: 'dashboard',
+                    pathMatch: 'full'
+                },
+                {
+                    path: 'dashboard',
+                    loadChildren: () =>
+                        import('./features/dashboard/dashboard-routes')
+                            .then(m => m.DASHBOARD_ROUTES)
+                },
+                {
+                    path: 'trips',
+                    loadChildren: () =>
+                        import('./features/trips/trips-routes')
+                            .then(m => m.TRIPS_ROUTES)
+                }
+            ]
+        },
+  // Ruta por defecto: raíz a login
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
   },
+  // EL COMODÍN SIEMPRE DEBE IR AL FINAL
   {
     path: '**',
     redirectTo: 'login'
