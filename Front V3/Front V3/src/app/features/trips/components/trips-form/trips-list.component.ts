386824f71903d0e35a97ff0964c6bafc84a19b0b
Gael Salazar <gaelsalazar2511@gmail.com>
2026-05-16 22:37:00 -0600

    Fix: URLs de QR apuntando a producción

--- Front V3/Front V3/src/app/features/trips/components/trips-form/trips-list.component.ts
+++ Front V3/Front V3/src/app/features/trips/components/trips-form/trips-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-trips-list',
@@ -19,7 +20,7 @@ export class TripsListComponent implements OnInit {
 
   loadTrips() {
     // Esta URL debe coincidir con la que creaste en Laminas API Tools
-    this.http.get<any>('http://localhost:8080/trips').subscribe({
+    this.http.get<any>(`${environment.apiUrl}/trips`).subscribe({
       next: (data) => {
         // Laminas suele devolver los datos dentro de una propiedad '_embedded'
         this.trips = data._embedded ? data._embedded.trips : data;
--- Front V3/Front V3/src/environments/environment.ts
+++ Front V3/Front V3/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://proyectosgt-por-angular-production.up.railway.app'
+  apiUrl: 'https://proyectosgt-por-angular-production.up.railway.app',
+  frontendUrl: 'https://proyecto-sgt-por-angular.vercel.app'
 };
